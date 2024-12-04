1d48411f90b2f1d36b488ab5db54b27519733aaf
JonathanBetPer <jonathanbetper@gmail.com>
2024-12-04 17:16:26 +0000

    Cambios css y js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let jugador2;
 let seleccion1 = false;
 let seleccion2 = false;
 
-function opcionEscogida(quien, respuesta, identificador) {
+function opcionEscogida(quien, respuesta) {
   alert(`El jugador ${quien} escogio ${respuesta}`);
   if (quien === 1) {
     jugador1 = respuesta;
@@ -17,6 +17,7 @@ function opcionEscogida(quien, respuesta, identificador) {
     determinarGanador(jugador1, jugador2);
   }
 }
+
 function bloquearOpciones(numJugador, opcionElegida) {
   const Jugador = document.getElementById(numJugador);
   const Botones = Jugador.querySelectorAll("button");
@@ -31,29 +32,47 @@ function determinarGanador(opcion1, opcion2) {
   } else if ((opcion1 === "tijera" && opcion2 === "papel") ||
         (opcion1 === "papel" && opcion2 === "piedra") ||
         (opcion1 === "piedra" && opcion2 === "tijera")) {
-    alert("Jugador 1 gana");
+    colorearGanador(1);
   } else {
-    alert("Jugador 2 gana");
+    colorearGanador(2);
   }
   MostrarReinciar();
 }
 
-function colorearGanador() {
-
+function colorearGanador(jugadorGanador) {
+  const divJugador = document.getElementById(jugadorGanador);
+  divJugador.classList.add("ganador");
 }
 
 function reiniciarJuego() {
-  const Botones = document.querySelectorAll("button");
-  Botones.forEach(boton => { boton.disabled = false; });
   jugador1 = null;
   jugador2 = null;
   seleccion1 = false;
   seleccion2 = false;
-  const laOpcionPulsada = document.getElementsByClassName("selected");
-  laOpcionPulsada.classList.remove("selected");
+
+  const botones = document.querySelectorAll("button");
+  botones.forEach(boton => {
+    boton.disabled = false;
+    boton.classList.remove("selected");
+  });
+
+  const opcionesSeleccionadas = document.querySelectorAll(".selected");
+  opcionesSeleccionadas.forEach(opcion => {
+    opcion.classList.remove("selected");
+  });
+
+  const divsJugador = document.querySelectorAll(".jugador");
+  divsJugador.forEach(div => {
+    div.classList.remove("ganador");
+  });
+
+  const reiniciarBtn = document.querySelector("button[onclick='reiniciarJuego()']");
+  if (reiniciarBtn) {
+    reiniciarBtn.remove();
+  }
 }
+
 function MostrarReinciar() {
   const nuestraWeb = document.body;
-
   nuestraWeb.innerHTML += "<button onClick=\"reiniciarJuego()\"> Reiniciar </button>";
 }
